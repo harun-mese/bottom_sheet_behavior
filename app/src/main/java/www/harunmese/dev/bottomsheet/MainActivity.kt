b0278a09403e0a8c4a432e0ee013f54ae025d7f8
harun-mese <harunmese13@gmail.com>
2024-02-28 14:34:40 +0300

    Initial commit

---
+++ app/src/main/java/www/harunmese/dev/bottomsheet/MainActivity.kt
@@ -0,0 +1,59 @@
+package www.harunmese.dev.bottomsheet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import www.harunmese.dev.bottomsheet.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // Bottom sheet behavior'ını tanımla
+        val bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet)
+
+        // Bottom sheet'in açılıp kapanmasını dinle
+        bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                // Durum değişikliğini burada işleyebilirsiniz
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                // Kaydırma olayını burada işleyebilirsiniz
+            }
+        })
+
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+
+        // Bottom sheet'i aç veya kapat
+       binding.toggleButton.setOnClickListener {
+            if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_HIDDEN) {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            } else {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+            }
+        }
+
+        binding.bb.setOnClickListener {
+            if (binding.textView.text == "Harun Mese") {
+                binding.textView.text = "Bottom Sheet Behavior"
+            } else {
+                binding.textView.text = "Harun Mese"
+            }
+        }
+
+
+
+
+    }
+
+
+
+}
